9ce52389c8a685559e09afb0878b62faa0473269
Matt Andreko <mandreko@gmail.com>
2012-06-18 20:47:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PwcrackWebui::Application.config.secret_token = '5056ffbd6d71d7cf6e5af03157528f470de33983e0331538261581ce5ea19449c8d92192d8240d7c3092254b0f155fb2d6a539b7d652297f6ad60ae385cd9ac9'
