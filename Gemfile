cacca5ec64325d96e7c9c0bf37af5af9e8d4c750
Matt Andreko <mandreko@gmail.com>
2012-06-18 21:19:45 -0400

    Switched to Mongoid

--- Gemfile
+++ Gemfile
@@ -1,11 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.6'
-
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
-gem 'sqlite3'
+gem 'mongoid'
 
 
 # Gems used only for assets and not required
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PwcrackWebui::Application.config.secret_token = '5056ffbd6d71d7cf6e5af03157528f470de33983e0331538261581ce5ea19449c8d92192d8240d7c3092254b0f155fb2d6a539b7d652297f6ad60ae385cd9ac9'
+PwcrackWebui::Application.config.secret_token = '2284c9be78fb6b84acfe65dfff121239a0c84484b8eb164706ef2ec15621b5cc1fa36937caed9f84eeee4920cbff8ae527f976441abed3fa1a55724aaf13d0f3'
