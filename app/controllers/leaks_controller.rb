2a9aa44306300ca49dcf764a078d02285d8cea89
Matt Andreko <mandreko@gmail.com>
2012-06-29 08:06:16 -0400

    Fixed tabbing
    Fixed the tabbing issue. Standardized on soft tabs at 2 spaces.

--- app/controllers/leaks_controller.rb
+++ app/controllers/leaks_controller.rb
@@ -1,35 +1,35 @@
 class LeaksController < ApplicationController
 
-	# GET /leaks
-	# GET /leaks.xml
-	def index
-		@leaks = Leaks.all
+  # GET /leaks
+  # GET /leaks.xml
+  def index
+    @leaks = Leaks.all
 
-		respond_to do |format|
-			format.html # index.html.erb
-			format.xml { render :xml => @leaks }
-		end
-	end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @leaks }
+    end
+  end
 
-	# GET /leaks/1
-	# GET /leaks/1.xml
-	def show
-		@leak = Leak.find(params[:id])
-	
-		respond_to do |format|
-			format.html # show.html.erb
-			format.xml { render :xml => @leak }
-		end
-	end
-	
-	# GET /leaks/new
-	def new
-	  @leak = Leak.new
-	  
-	  respond_to do |format|
-	          format.html # new.html.erb
-	          format.xml { render :xml => @leak }
-	         end
+  # GET /leaks/1
+  # GET /leaks/1.xml
+  def show
+    @leak = Leak.find(params[:id])
+  
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml { render :xml => @leak }
+    end
+  end
+  
+  # GET /leaks/new
+  def new
+    @leak = Leak.new
+    
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml { render :xml => @leak }
+    end
   end
   
   # POST /leaks/create
@@ -37,7 +37,7 @@ class LeaksController < ApplicationController
     @leak = Leak.new(params[:leak])
     
     # Remember the original filename, even after we nil out the file
-	  @leak.original_filename = @leak.file_file_name
+    @leak.original_filename = @leak.file_file_name
     #render :text => @leak.inspect
     
     if @leak.save
--- app/models/leak.rb
+++ app/models/leak.rb
@@ -1,22 +1,22 @@
 class Leak
-	include Mongoid::Document
-	include Mongoid::Paperclip
-	
-	field :original_filename
-	field :source
-	
-	has_mongoid_attached_file :file
+  include Mongoid::Document
+  include Mongoid::Paperclip
+  
+  field :original_filename
+  field :source
+  
+  has_mongoid_attached_file :file
 
-	validates :original_filename, :presence => true
+  validates :original_filename, :presence => true
 
-	references_many :passwords	
-	
-	def load_data
-	  
-	  File.open(self.file.path).each do |line|
-	    
-	    pattern = /^(?<hash>[a-zA-Z\d]*):?(?<salt>[a-zA-Z\d]*)?/
-	    match = pattern.match line.strip
+  references_many :passwords  
+  
+  def load_data
+    
+    File.open(self.file.path).each do |line|
+      
+      pattern = /^(?<hash>[a-zA-Z\d]*):?(?<salt>[a-zA-Z\d]*)?/
+      match = pattern.match line.strip
       
       pass = Password.new
       pass.leakfilename = self.original_filename
@@ -29,7 +29,7 @@ class Leak
      
      self.file = nil
      self.save
-	  
+    
   end
   # Always call load_data in the background
   handle_asynchronously :load_data
